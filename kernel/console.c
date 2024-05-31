87a5c51528f25ca4fd317f7b92ccf101acfeec0a
tlucanti <prototype3628800@yandex.ru>
2024-06-01 01:22:25 +0300

    console: add number printing functions
     - add signed and unsigned numbers printing to console functions
    
    Signed-off-by: tlucanti <prototype3628800@yandex.ru>

--- kernel/console.c
+++ kernel/console.c
@@ -1,4 +1,5 @@
 
+#include <kernel/types.h>
 #include <sys/uart.h>
 
 void console_init(void)
@@ -13,17 +14,67 @@ char console_getc(void)
 	return uart_getc();
 }
 
-void console_putc(char c)
+uint console_putc(char c)
 {
 	uart_putc(c);
 	uart_write_barrier();
+	return 1;
 }
 
-void console_puts(const char *s)
+uint console_puts(const char *s)
 {
-	while (*s) {
-		console_putc(*s);
-		s++;
+	uint n = 0;
+	while (s[n]) {
+		console_putc(s[n]);
+		n++;
 	}
+	return n;
 }
 
+uint console_putint(int64 x)
+{
+	char buf[22];
+	uint i = 21;
+	const bool negative = x < 0;
+
+	if (x == 0) {
+		console_putc('0');
+		return 1;
+	}
+
+	buf[i--] = '\0';
+	while (x) {
+		if (negative) {
+			buf[i--] = -(x % 10) + '0';
+		} else {
+			buf[i--] = x % 10 + '0';
+		}
+		x /= 10;
+	}
+	if (negative) {
+		buf[i--] = '-';
+	}
+
+	console_puts(buf + i + 1);
+	return 20 - i;
+}
+
+uint console_putuint(uint64 x)
+{
+	char buf[22];
+	uint i = 21;
+
+	if (x == 0) {
+		console_putc('0');
+		return 1;
+	}
+
+	buf[i--] = 0;
+	while (x) {
+		buf[i--] = x % 10 + '0';
+		x /= 10;
+	}
+
+	console_puts(buf + i + 1);
+	return 20 - i;
+}
