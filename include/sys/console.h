0269b2968c94ef6041531de1bd0234cb86e414b8
tlucanti <prototype3628800@yandex.ru>
2024-06-01 01:22:25 +0300

    console: add number printing functions
     - add signed and unsigned numbers printing to console functions
    
    Signed-off-by: tlucanti <prototype3628800@yandex.ru>

--- include/sys/console.h
+++ include/sys/console.h
@@ -2,10 +2,14 @@
 #ifndef KERNEL_CONSOLE_H
 #define KERNEL_CONSOLE_H
 
-void console_init(void);
+#include <kernel/types.h>
+
+uint console_init(void);
 char console_getc(void);
-void console_putc(char c);
-void console_puts(const char *s);
+uint console_putc(char c);
+uint console_puts(const char *s);
+uint console_putint(int64 x);
+uint console_putuint(uint64 x);
 
 #endif /* KERNEL_CONSOLE_H */
 
