6bedf735805f3fee51c8210fb7b1b148e8ad7ca2
Konstantin Semichastnov <konstantin.semichastnov@huawei-partners.com>
2024-06-05 18:52:45 +0800

    kernel: add userspace
    Signed-off-by: Konstantin Semichastnov <konstantin.semichastnov@huawei-partners.com>

--- kernel/start_kernel.c
+++ kernel/start_kernel.c
@@ -8,18 +8,26 @@ __aligned(16)
 uint8 _kernel_stack[4096];
 
 extern void trap_init(void);
+extern void init(void);
+__noreturn void start_user(void);
 
+__noreturn
 void start_kernel(void)
 {
 	console_init();
+	console_puts("=== kernel ===\n");
+	console_puts("kernel starting\n");
 	trap_init();
-
 	rv_ecall();
 
-	while (true) {
-		console_puts("read >> ");
-		console_putc(console_getc());
-		console_putc('\n');
-	}
+	start_user();
 }
 
+__noreturn
+void start_user(void)
+{
+	csr_write_mepc((uint64)init); // set return address for mret
+	csr_write_mstatus_mpp(MSTATUS_MPP_M); // set previous mode to M mode for mret
+	csr_write_mstatus_mpie(true); // set previous interrupt enable to true from mret
+	rv_mret();
+}
