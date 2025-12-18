6a117ee58826d796b33222d08e011a7fa87b725c
Konstantin Semichastnov <k.semichastnov@syntacore.com>
2025-12-18 23:25:26 +0300

    kernel: add bit ops header
    add header with miscellaneous utilities
    
    Signed-off-by: Konstantin Semichastnov <k.semichastnov@syntacore.com>

---
+++ include/kernel/ops.h
@@ -0,0 +1,51 @@
+
+#ifndef KERNEL_OPS_H
+#define KERNEL_OPS_H
+
+#include <kernel/types.h>
+
+#define BIT(x) ((uint64)1 << (x))
+#define MASK(offset, width) ((BIT(width) - 1) << offset)
+
+static inline uint8 read8(uint64 addr)
+{
+	volatile uint8 *ptr = (uint8 *)addr;
+
+	return *ptr;
+}
+
+static inline void write8(uint64 addr, uint8 value)
+{
+	volatile uint8 *ptr = (uint8 *)addr;
+
+	*ptr = value;
+}
+
+static inline uint64 extract(uint64 value, uint64 shift, uint8 width)
+{
+	if (shift >= 64) {
+		return 0;
+	}
+
+	value >>= shift;
+	return value & MASK(0, width);
+}
+
+static inline uint64 deposit(uint64 orig, uint64 shift, uint8 width, uint64 value)
+{
+	if (shift >= 64) {
+		return orig;
+	}
+
+	/* remove extra bits from value */
+	value &= MASK(0, width);
+	value <<= shift;
+
+	/* clear bits that will be rewritten from orig */
+	orig &= ~MASK(0, width);
+
+	return orig | value;
+}
+
+#endif /* KERNEL_OPS_H */
+
