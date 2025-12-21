6ace42f25839bacef42aef54e5b9634309016ae7
Konstantin Semichastnov <k.semichastnov@syntacore.com>
2025-12-21 22:05:31 +0300

    kernel: update attributes
     - add __noreturn attribute
     - add PAGE_SIZE macro
    
    Signed-off-by: Konstantin Semichastnov <k.semichastnov@syntacore.com>

--- include/kernel/attributes.h
+++ include/kernel/attributes.h
@@ -2,7 +2,11 @@
 #ifndef KERNEL_ATTRIBUTES_H
 #define KERNEL_ATTRIBUTES_H
 
+#define PAGE_SIZE 4096
+
 #define __aligned(x) __attribute__((__aligned__(x)))
+
+#define __noreturn __attribute__((__noreturn__))
 
 #endif /* KERNEL_ATTRIBUTES_H */
 
